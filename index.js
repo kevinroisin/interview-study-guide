e03b73c95cbeb7b0c56a15419b7603a207bda500
Kevin Roisin <kroisin@us.ibm.com>
2017-02-11 21:24:56 -0500

    way too much work to make the printable page

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ let express = require('express');
 var bodyParser = require('body-parser')
 let highlightjs = require('highlight.js');
 let marked = require('marked');
-let Bluebird = require('bluebird');
 let path = require('path');
+let Bluebird = require('bluebird');
 let fs = Bluebird.promisifyAll(require("fs"));
 let Case = require('case');
+let _ = require('lodash');
+let async = require('async');
 
 let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,8 +29,22 @@ app.get('/', async (req, res) => {
   res.render('problem', { title: 'Interview Study Guide', menu: menu, landing: landing });
 });
 
-app.post('/print', async (req, res) => {
-  res.json(req.body);
+app.post('/print', (req, res) => {
+  const BASE_PATH = path.join(__dirname, 'problems');
+
+  // load all of the problems into an array
+  let problemPaths = _.map(req.body, (problem, key) => {
+    return {
+      path: key,
+      name: Case.title(key)
+    }
+  });
+
+  loadFiles(problemPaths, (problemsToPrint) => {
+    // display all of the problems in a pug template
+    res.render('print', { title: 'Interview Study Guide',  problems: problemsToPrint });
+  });
+
 });
 
 app.get('/problems/:problem', async (req, res) => {
@@ -52,4 +68,23 @@ app.listen(process.env.PORT || 3000, () => {
 async function readFile(path) {
   let file = await fs.readFileAsync(path, 'utf8');
   return file;
+}
+
+function loadFiles(problemPaths, callback) {
+  const BASE_PATH = path.join(__dirname, 'problems');
+
+  async.map(problemPaths, function(problem, cb) {
+    let problemPath = path.join(BASE_PATH, problem.path, 'E.md');
+
+    fs.readFile(problemPath, 'utf8', (err, problemFile) => {
+      if(err) {
+        cb(err || 'error loading file ' + problem.path);
+      } else {
+        problem.explanation = marked(problemFile);
+        cb(null, problem);
+      }
+    });
+  }, function(err, problems) {
+    callback(problems);
+  });
 }
--- index.md
+++ index.md
@@ -1,3 +1,2 @@
 # Interview Study Guide
-
 The ultimate study guide for technical interviews. It's just not very ultimate yet.
